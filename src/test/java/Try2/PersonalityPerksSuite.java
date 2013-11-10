0733eba100d8c67cb5935868acb182b635acf9e3
alexyem0222 <alex.yem22@gmail.com>
2013-11-10 12:09:23 -0800

    Added all

---
+++ src/test/java/Try2/PersonalityPerksSuite.java
@@ -0,0 +1,20 @@
+package Try2;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+
+
+import Try2.PersonalityAssertLinks;
+import Try2.PersonalityPerks1;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        PersonalityAssertLinks.class,
+        PersonalityPerks1.class
+})
+
+public class PersonalityPerksSuite {
+}
