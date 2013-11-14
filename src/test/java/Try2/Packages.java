2827dee5a8d22ebb2e1401da3a672278ba3c57d1
alexyem0222 <alex.yem22@gmail.com>
2013-11-13 18:01:37 -0800

    Add new files

--- src/test/java/Try2/Packages.java
+++ src/test/java/Try2/Packages.java
@@ -1,11 +1,35 @@
 package Try2;
 
-/**
- * Created with IntelliJ IDEA.
- * User: Alex
- * Date: 11/13/13
- * Time: 5:45 PM
- * To change this template use File | Settings | File Templates.
- */
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertFalse;
+import static junit.framework.Assert.assertTrue;
+
+
 public class Packages {
+    WebDriver driver = new FirefoxDriver();
+
+    @Before
+    public void setup(){
+        driver.get("http://www.personalityhotels.com");
+    }
+
+    @Test
+    public void clickonpackages(){
+        WebElement packages =driver.findElement(By.cssSelector("html body#home.tmpl-generic div.container div#container-content div#col-1 div.tab-menu ul li a#t-packages"));
+        packages.click();
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        driver.quit();
+    }
 }
