a8adbfeeb5775b5e1cb54fd2fa3ca365eb4113ea
alexyem0222 <alex.yem22@gmail.com>
2013-11-13 17:59:03 -0800

    Add new .java class file

---
+++ src/test/java/Try2/Packages.java
@@ -0,0 +1,11 @@
+package Try2;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Alex
+ * Date: 11/13/13
+ * Time: 5:45 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Packages {
+}
