12c1285bb80c74c7e4222d618f36300778989349
alexyem0222 <alex.yem22@gmail.com>
2013-11-16 20:13:19 -0800

    Added logout feature

--- src/test/java/Try2/RegisteredUserLogin.java
+++ src/test/java/Try2/RegisteredUserLogin.java
@@ -8,6 +8,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertFalse;
@@ -16,24 +18,30 @@ import static org.junit.Assert.assertThat;
 
 public class RegisteredUserLogin {
     WebDriver driver = new FirefoxDriver();
+    WebDriverWait wait = new WebDriverWait(driver, 10);
 
     @Before
-    public void setup(){
-         driver.get("http://www.personalityperks.com");
+    public void setup() {
+        driver.get("http://www.personalityperks.com");
+
     }
 
     @Test
-    public void login(){
-       WebElement email = driver.findElement(By.id("loginEmail"));
-       WebElement password= driver.findElement(By.id("loginPassword"));
-       WebElement loginbutton = driver.findElement(By.xpath("/html/body/div/div[2]/div/form/fieldset/div/button"));
-       email.sendKeys("love2@love.com");
-       password.sendKeys("love@22");
-       loginbutton.click();
+    public void login() {
+        WebElement email = driver.findElement(By.id("loginEmail"));
+        WebElement password = driver.findElement(By.id("loginPassword"));
+        WebElement loginbutton = driver.findElement(By.xpath("/html/body/div/div[2]/div/form/fieldset/div/button"));
+        email.sendKeys("love2@love.com");
+        password.sendKeys("love@22");
+        loginbutton.click();
+        //WebElement logoutlink = driver.findElement(By.xpath("/html/body/div/div[2]/div/div/ul/li[5]/a"));
+        //Assert.assertTrue("Verify logout link is displayed", logoutlink.isDisplayed());
+        WebElement logoutlink = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("/html/body/div/div[2]/div/div/ul/li[5]/a")));
+        logoutlink.click();
     }
 
     @After
-    public void tearDown() throws Exception{
+    public void tearDown() throws Exception {
         driver.quit();
     }
 }
