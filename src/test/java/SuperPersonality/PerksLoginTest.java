ad109732dd6cae915637190b35c875c394c0cc67
alexyem0222 <alex.yem22@gmail.com>
2014-03-25 16:52:59 -0700

    Trying excel test

--- src/test/java/SuperPersonality/PerksLoginTest.java
+++ src/test/java/SuperPersonality/PerksLoginTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
+import Various.SignUpPageAllFields;
 import org.databene.benerator.anno.Source;
 import org.databene.feed4junit.Feeder;
 import org.junit.After;
@@ -16,20 +17,31 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 @RunWith(Feeder.class)
 public class PerksLoginTest {
 
-   private FirefoxDriver driver;
+    private FirefoxDriver driver;
 
     @Before
     public void setupSelenium() {
-       driver = new FirefoxDriver();
-   }
+        driver = new FirefoxDriver();
+    }
 
-    @Test
+   /* @Test
     @Source ("C:\\\\Users\\\\Alex\\\\Desktop\\\\personalityperks.xls")
     public void TestDataLogin(String email, String password){
         LoginBase loginbase = LoginBase.navigateTo(driver);
         PersonalityPerks pp = loginbase.login(email, password);
-    }
+    }  */
 
+    @Test
+    @Source ("C:\\\\Users\\\\Alex\\\\Desktop\\\\perksfull.xls")
+    public void TestSignUp(String firstname, String lastname, String mailingaddress, String bedtype){
+        SignUpPageAllFields signup = SignUpPageAllFields.navigateTo(driver);
+        signup.enterFirstName(firstname);
+        signup.enterLastName(lastname);
+        signup.selectMailingOption(mailingaddress);
+        signup.specialAccessNone();
+        signup.selectBedType(bedtype);
+
+    }
     @After
     public void tearDown() throws Exception {
         driver.quit();
