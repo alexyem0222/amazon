226228954c53ceaec9a2b9e8e7e0cb4fee1f9165
alexyem0222 <alex.yem22@gmail.com>
2014-03-27 13:15:03 -0700

    Come on now

--- src/test/java/Various/SampleSuite.java
+++ src/test/java/Various/SampleSuite.java
@@ -3,10 +3,6 @@ package Various;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
-
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
        PerksLoginBase.class,
