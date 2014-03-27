7b28b4debc7bcd8fc031e2459cfe9c55966d7455
alexyem0222 <alex.yem22@gmail.com>
2014-03-26 19:52:36 -0700

    Trying

--- src/test/java/SuperPersonality/PerskLoginTestSuiteExcel.java
+++ src/test/java/SuperPersonality/PerskLoginTestSuiteExcel.java
@@ -5,7 +5,6 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 import SuperPersonality.PerksLoginTest;
-import SuperPersonality.PersonalityPerks;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
