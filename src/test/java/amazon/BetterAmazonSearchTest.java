885241da42ca75fca1afe212e9303ba06fda6765
alexyem0222 <alex.yem22@gmail.com>
2014-08-17 16:25:14 -0700

    Added another test

--- src/test/java/amazon/BetterAmazonSearchTest.java
+++ src/test/java/amazon/BetterAmazonSearchTest.java
@@ -5,9 +5,12 @@ import static org.junit.Assert.assertThat;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class BetterAmazonSearchTest {
 
     private FirefoxDriver driver;
@@ -17,12 +20,22 @@ public class BetterAmazonSearchTest {
         driver = new FirefoxDriver();
     }
 
-
     @Test
     public void search_amazon() throws InterruptedException {
         AmazonHomePage homePage = AmazonHomePage.navigateTo(driver);
         AmazonSearchResultsPage resultsPage = homePage.searchFor("robin");
+       // try{
         assertThat(resultsPage.getTopResultTitle(), is("Robin (comics) - Wikipedia, the free encyclopedi"));
+   // } catch (Throwable e) {
+         // System.out.println("The result title actual and expected DO NOT match" + e.getMessage());
+       // }
+    }
+
+    @Test
+    public void search_yahoo() throws InterruptedException {
+        AmazonHomePage amazonHomePage = AmazonHomePage.navigateTo(driver);
+        AmazonSearchResultsPage amazonSearchResultsPage = amazonHomePage.searchFor("selenium");
+        assertThat(amazonSearchResultsPage.getTopResultTitle(), is("Selenium - Official Site"));
     }
 
     @After
