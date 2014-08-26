8b6bbbcbbc392b90a132a1fc2c3e572dd173cd05
alexyem0222 <alex.yem22@gmail.com>
2014-08-25 20:49:28 -0700

    another try

--- src/test/java/amazon/BetterAmazonSearchTest.java
+++ src/test/java/amazon/BetterAmazonSearchTest.java
@@ -3,14 +3,13 @@ package amazon;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
-import org.jcp.xml.dsig.internal.dom.Utils;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+//import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
