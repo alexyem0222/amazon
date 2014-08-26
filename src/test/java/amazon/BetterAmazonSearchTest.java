309481059e3a1891dd76470ba13b187ea28a794b
alexyem0222 <alex.yem22@gmail.com>
2014-08-25 21:10:14 -0700

    comeone

--- src/test/java/amazon/BetterAmazonSearchTest.java
+++ src/test/java/amazon/BetterAmazonSearchTest.java
@@ -25,13 +25,13 @@ public class BetterAmazonSearchTest {
 
     @Before
     public void setupSelenium() throws MalformedURLException {
-        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
-        desiredCapabilities.setBrowserName(System.getenv("Android"));
-        desiredCapabilities.setVersion(System.getenv("4.4"));
-        desiredCapabilities.setCapability(CapabilityType.PLATFORM, System.getenv("SELENIUM_PLATFORM"));
+        DesiredCapabilities caps = DesiredCapabilities.firefox();
+        caps.setCapability("platform", "Windows 8.1");
+        caps.setCapability("version", "31");
+        caps.setCapability(CapabilityType.PLATFORM, System.getenv("SELENIUM_PLATFORM"));
         driver = new RemoteWebDriver(
                 new URL("http://alex_yem22:e8760d07-6354-4530-b74f-c91e9556f7f6@ondemand.saucelabs.com:80/wd/hub"),
-                desiredCapabilities);
+                caps);
     }
 
     @Test
