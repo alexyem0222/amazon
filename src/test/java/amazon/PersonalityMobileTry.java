c3080d52e578131afd2376cd177df2cfdc7be905
alexyem0222 <alex.yem22@gmail.com>
2014-12-19 20:32:26 -0800

    another try

--- src/test/java/amazon/PersonalityMobileTry.java
+++ src/test/java/amazon/PersonalityMobileTry.java
@@ -1,56 +1,45 @@
 package amazon;
 
+
 import org.junit.After;
 import org.junit.Before;
-import org.junit.FixMethodOrder;
 import org.junit.Test;
-import org.junit.runners.MethodSorters;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.net.MalformedURLException;
 import java.net.URL;
 
-import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.*;
 
-@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class PersonalityMobileTry {
 
-    public WebDriver driver;
+    private WebDriver driver;
+
 
     @Before
-    public void setupSelenium() throws MalformedURLException {
-        DesiredCapabilities caps = DesiredCapabilities.android();
-        caps.setCapability("platform", "Linux");
-        caps.setCapability("version", "4.4");
-        caps.setCapability("deviceName", "Samsung Galaxy S3 Emulator");
-        caps.setCapability("device-orientation", "portrait");
-        driver = new RemoteWebDriver(
-                new URL("http://alex_yem22:e8760d07-6354-4530-b74f-c91e9556f7f6@ondemand.saucelabs.com:80/wd/hub"),
-                caps);
+    public void setUp() throws Exception {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("deviceName", "iPhone Simulator");
+        capabilities.setCapability("platformName", "iOS");
+        capabilities.setCapability("platformVersion", "7.1");
+        capabilities.setCapability("browserName", "safari");
+        driver = new RemoteWebDriver(new URL("http://alex_yem22:e8760d07-6354-4530-b74f-c91e9556f7f6@ondemand.saucelabs.com:80/wd/hub"),
+                capabilities);
 
     }
+
 
     @Test
-    public void personalityTry(){
-        WebDriverWait wait = new WebDriverWait(driver, 30);
-        driver.get("http://m.yahoo.com");
-       // wait.until(ExpectedConditions.titleIs("Yahoo for Mobile. Your favorite Yahoo services, on your phone."));
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("p")));
-        WebElement yahooSearchField = driver.findElement(By.name("p"));
-        yahooSearchField.sendKeys("mama");
+    public void runTest() throws Exception {
+        driver.get("http://www.yahoo.com");
+        WebElement searchBox = driver.findElement(By.name("p"));
+        searchBox.sendKeys("test");
     }
 
-
     @After
-
     public void printSessionId() {
         String message = String.format("SauceOnDemandSessionID=%1$s job-name=%2$s",
                 (((RemoteWebDriver) driver).getSessionId()).toString(), "PersonalityMobileTest");
@@ -59,4 +48,3 @@ public class PersonalityMobileTry {
         driver.quit();
     }
 }
-
