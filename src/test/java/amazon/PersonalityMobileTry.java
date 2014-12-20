f77394ff079c1f9df80571686f68f0c093ecc5f3
alexyem0222 <alex.yem22@gmail.com>
2014-12-19 20:43:42 -0800

    added close

--- src/test/java/amazon/PersonalityMobileTry.java
+++ src/test/java/amazon/PersonalityMobileTry.java
@@ -44,7 +44,7 @@ public class PersonalityMobileTry {
         String message = String.format("SauceOnDemandSessionID=%1$s job-name=%2$s",
                 (((RemoteWebDriver) driver).getSessionId()).toString(), "PersonalityMobileTest");
         System.out.println(message);
-        //driver.close();
+        driver.close();
         driver.quit();
     }
 }
